5caa982307b711e3419112450be68338232c71c4
Desmond Germans <desmond@germansmedia.nl>
2023-07-25 16:15:02 +0200

    got some result resembling an actual MandelBulb

--- shaders/engine.glsl
+++ shaders/engine.glsl
@@ -10,33 +10,102 @@ layout (binding = 0) readonly uniform WhatIsThisName {
 
 layout (binding = 1) writeonly uniform image2D out_frame;
 
-#define MAX_STEPS 100
+// basic types for the calculations
+#define FLOAT float
+#define VEC2 vec2
+#define VEC3 vec3
+
+// step size (initial for SDF)
+#define STEP_SIZE 0.01
+
+// max number of steps
+#define MAX_STEPS 1000
+
+// when to stop searching
 #define CLOSEST 0.0001
-#define MAX_DISTANCE 10000
+#define MAX_DISTANCE 10.0
 
-float scene(vec3 p) {
-    vec3 sphere1_center = vec3(0.0,0.0,600.0);
-    float sphere1_radius = 20.0;
-    float d = length(sphere1_center - p) - sphere1_radius;
+// WHEN YOU CAN'T CALCULATE THE SDF:
 
-    vec3 sphere2_center = vec3(-80.0,20.0,700.0);
-    float sphere2_radius = 60.0;
-    d = min(d,length(sphere2_center - p) - sphere2_radius);
+#define MAX_ITERATION 100
+#define ESCAPE_DISTANCE 2.0
 
-    vec3 sphere3_center = vec3(70.0,-60.0,400.0);
-    float sphere3_radius = 40.0;
-    return min(d,length(sphere3_center - p) - sphere3_radius);
+int calc_bulb(VEC3 p,VEC3 center) {
+    VEC3 v = p - center;
+    for(int i = 1; i <= MAX_ITERATION; i++) {
+        FLOAT r = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+        FLOAT phi = atan(v.y,v.x);
+        FLOAT theta = acos(v.z / r);
+        FLOAT r2 = r * r;
+        FLOAT r4 = r2 * r2;
+        FLOAT r8 = r4 * r4;
+        FLOAT s8t = sin(8 * theta);
+        FLOAT nx = s8t * cos(8 * phi);
+        FLOAT ny = s8t * sin(8 * phi);
+        FLOAT nz = cos(8 * theta);
+        v = r8 * VEC3(nx,ny,nz) + p - center;
+        if(length(v) > ESCAPE_DISTANCE) {
+            return i;
+        }
+    }
+    return 0;
 }
 
-vec3 normal(vec3 p) {
-    vec2 e = vec2(1.0,-1.0) * 0.0005;
+vec4 no_sdf(VEC3 start,VEC3 dp) {
+    VEC3 p = start;
+    FLOAT depth = 1.0;
+    for (int i = 0; i < MAX_STEPS; i++) {
+        p += STEP_SIZE * dp;
+        depth += STEP_SIZE;
+        if (depth > MAX_DISTANCE) {
+            return vec4(0.1,0.1,0.1,1.0);
+        }
+        if (calc_bulb(p,VEC3(0.0,1.0,5.0)) == 0) {
+            return fract(10.0 * depth) * vec4(1.0,0.5,0.1,1.0);
+        }
+    }
+    return vec4(0.1,0.1,0.1,1.0);
+}
+
+// WHEN YOU CAN CALCULATE THE SDF
+
+FLOAT sphere(VEC3 p,VEC3 center,FLOAT radius) {
+    return length(center - p) - radius;
+}
+
+FLOAT calc_sdf(VEC3 p) {
+    return sphere(p,VEC3(0.0,1.0,5.0),1.0);
+}
+
+VEC3 normal(VEC3 p) {
+    VEC2 e = VEC2(1.0,-1.0) * 0.0005;
     return normalize(
-        e.xyy * scene(p + e.xyy) +
-        e.yyx * scene(p + e.yyx) +
-        e.yxy * scene(p + e.yxy) +
-        e.xxx * scene(p + e.xxx)
+        e.xyy * calc_sdf(p + e.xyy) +
+        e.yyx * calc_sdf(p + e.yyx) +
+        e.yxy * calc_sdf(p + e.yxy) +
+        e.xxx * calc_sdf(p + e.xxx)
     );
 }
+
+vec4 sdf(VEC3 p,VEC3 dp) {
+    FLOAT depth = 1.0;
+    FLOAT d = STEP_SIZE;
+    for (int i = 0; i < MAX_STEPS; i++) {
+        p += d * dp;
+        d = calc_sdf(p);
+        depth += d;
+        if (depth > MAX_DISTANCE) {
+            return vec4(0.1,0.1,0.1,1.0);
+        }
+        if (d < CLOSEST) {
+            vec3 n = normal(p);
+            return vec4(0.5 + 0.5 * n.x,0.5 + 0.5 * n.y,0.5 + 0.5 * n.z,1.0);
+        }
+    }
+    return vec4(0.1,0.1,0.1,1.0);
+}
+
+// MAIN
 
 void main() {
 
@@ -44,40 +113,14 @@ void main() {
     ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
 
     // starting point
-    vec3 p = vec3(0.0,0.0,0.0);
-    float depth = 1.0;
+    VEC3 p = VEC3(0.0,0.0,0.0);
 
     // starting direction vector
-    vec3 dp = normalize(vec3(float(coord.x),float(coord.y),0.0) - state_eye.xyz);
-    float d = 1.0;
+    VEC3 dp = normalize(VEC3(FLOAT(coord.x),FLOAT(coord.y),0.0) - state_eye.xyz);
 
-    // iterate at most MAX_STEPS
-    for (int i = 0; i < MAX_STEPS; i++) {
+    // DO IT!
+    //vec4 color = sdf(p,dp);
+    vec4 color = no_sdf(p,dp);
 
-        // march
-        p += d * dp;
-
-        // sample scene
-        d = scene(p);
-        depth += d;
-
-        // if too far away, write dark gray and exit
-        if (depth > MAX_DISTANCE) {
-            imageStore(out_frame,coord,vec4(0.1,0.1,0.1,1.0));
-            return;
-        }
-
-        // if found something...
-        if (d < CLOSEST) {
-
-            // calculate normal
-            vec3 n = normal(p);
-
-            imageStore(out_frame,coord,vec4(0.5 + 0.5 * n.x,0.5 + 0.5 * n.y,0.5 + 0.5 * n.z,1.0));
-            return;
-        }
-    }
-
-    // if no object found, write dark gray and exit
-    imageStore(out_frame,coord,vec4(0.1,0.1,0.1,1.0));
+    imageStore(out_frame,coord,color);
 }
